766773662e7aa6bcde6b6fc55572bedfe83760c4
Ivan Juarez <ivan@blindgazer.com>
2026-05-15 02:27:58 -0600

    Publish docs site

--- src/components/common/FloatingBar.jsx
+++
@@ -1,7 +0,0 @@
-export function FloatingBar({ ariaLabel, children, className = "", orientation = "vertical" }) {
-  return (
-    <div className={`floating-bar ${orientation} ${className}`} aria-label={ariaLabel}>
-      {children}
-    </div>
-  );
-}
--- src/components/common/Footer.jsx
+++
@@ -1,3 +0,0 @@
-export function Footer() {
-  return <footer className="app-footer">Palette Recolor Studio</footer>;
-}
